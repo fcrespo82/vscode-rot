7796509fcea91c33df58bacd296d21662f6f5929
Fernando Crespo <fernando82@gmail.com>
2016-01-29 16:48:40 -0200

    Updated extension

--- src/extension.ts
+++ src/extension.ts
@@ -2,41 +2,41 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-var statusBarItem;
+var statusBarItem
 export function activate(context: vscode.ExtensionContext) {
 
-    var a = vscode.workspace.getConfiguration('rot')
-    console.log(a)
-    statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
-    statusBarItem.command = "extension.rot.13";
-    statusBarItem.text = "ROT 13";
-    statusBarItem.show();
+    statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right)
     
-    var rot13Command = vscode.commands.registerCommand('extension.rot.13', () => {
-        commandFunc(rot13);
-    });
-    context.subscriptions.push(rot13Command);
+    function _updateStatusBar() {
+        var showStatusBarButton = vscode.workspace.getConfiguration("rot").get<boolean>("showStatusBarButton")
+        var statusBarCommand = vscode.workspace.getConfiguration("rot").get<string>("statusBarCommand")
 
-    var rot47Command = vscode.commands.registerCommand('extension.rot.47', () => {
-        commandFunc(rot47);
-    });
-    context.subscriptions.push(rot47Command);
-       
-    var chooseCommand = vscode.commands.registerCommand('extension.chooseCommand', () => {
-        var answer = vscode.window.showQuickPick(["ROT 13", "ROT 47"]);
-        
-        answer.then(function(value) {
-            if (value == "ROT 13") {
-                statusBarItem.command = "extension.rot.13";
-                statusBarItem.text = "ROT 13";
-            } else if (value == "ROT 47") {
-                statusBarItem.command = "extension.rot.47";
-                statusBarItem.text = "ROT 47";
-            }
-        });
-        
-    });
-    context.subscriptions.push(chooseCommand);
+        statusBarItem.command = statusBarCommand
+        if (statusBarCommand == "rot.13") {
+            statusBarItem.text = "ROT 13"
+        } else if (statusBarCommand == "rot.47") {
+            statusBarItem.text = "ROT 47"
+        }
+        if (showStatusBarButton) {
+            statusBarItem.show()
+        } else {
+            statusBarItem.hide()
+        }
+    }
+    
+    _updateStatusBar()
+
+    var rot13Command = vscode.commands.registerCommand('rot.13', () => {
+        commandFunc(rot13)
+    })
+    context.subscriptions.push(rot13Command)
+
+    var rot47Command = vscode.commands.registerCommand('rot.47', () => {
+        commandFunc(rot47)
+    })
+    context.subscriptions.push(rot47Command)
+
+    var configListener = vscode.workspace.onDidChangeConfiguration(_updateStatusBar, this, context.subscriptions)
 }
 
 export function deactivate() {
@@ -51,11 +51,11 @@ function rot13(text) {
 function rot47(text) {
     var s = [];
     for (var i = 0; i < text.length; i++) {
-        var j = text.charCodeAt(i);
+        var j = text.charCodeAt(i)
         if ((j >= 33) && (j <= 126)) {
             s[i] = String.fromCharCode(33 + ((j + 14) % 94));
         } else {
-            s[i] = String.fromCharCode(j);
+            s[i] = String.fromCharCode(j)
         }
     }
     return s.join('');
@@ -71,8 +71,8 @@ function commandFunc(rotFunc) {
 
     editor.edit((edits) => {
         selections.forEach(selection => {
-            var text = editor.document.getText(selection);
-            edits.replace(selection, rotFunc(text));
-        });
+            var text = editor.document.getText(selection)
+            edits.replace(selection, rotFunc(text))
+        })
     })
 }
